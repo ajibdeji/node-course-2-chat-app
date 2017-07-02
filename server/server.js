39beccebf112d04dc0c4149bc441033f0e453be6
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-07-01 21:14:51 -0400

    add connected or disconnected event

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.error('user was disconnected');
+    });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('app started on port 3000');
 });
