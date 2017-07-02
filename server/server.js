30b328c8bc94cad1c32d1b5f4b37e54da42a56f1
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-07-01 20:47:20 -0400

    correct port variable

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('app started on port 3000');
 });
