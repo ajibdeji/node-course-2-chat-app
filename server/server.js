e77acc64b02adb291516aa7e5c6c7da825e21e67
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-07-02 07:00:59 -0400

    create generateMessage Utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const { generateMessage } = require('./utils/mesage');
 
 var app = express();
 var server = http.createServer(app);
@@ -15,37 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // socket.emit('newEmail', {
-    //     from: "anemailaddy2yahoo.com",
-    //     text: "some email body",
-    //     createdAt: new Date().getTime()
-    // });
 
-    // socket.on('createEmail', function(newEmail) {
-    //     console.log('New email', newEmail);
-    // });
-    // socket.on('disconnect', () => {
-    //     console.error('user was disconnected');
-    // });
-
-    socket.emit('newMessage', {
-        from: "Oke",
-        text: "hey",
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));;
 
     socket.on('createMessage', function(message) {
         console.log('New Message', message);
-        // io.emit('newMessage', {
+        io.emit('newMessage', generateMessage(from, text));
+        // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
         //     createdAt: new Date().getTime()
-        // });
-        socket.broadcast.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        // })
     });
 
 
