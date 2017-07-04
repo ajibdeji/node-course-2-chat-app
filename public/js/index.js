85d5968eb2a0336d05ef69842d88ded07d1d119f
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-07-04 17:05:58 -0400

    added mustache templating

--- public/js/index.js
+++ public/js/index.js
@@ -20,10 +20,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-    $('#messages').append(li);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+    // var li = $('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+    // $('#messages').append(li);
 });
 
 $('#message-form').on('submit', function(e) {
@@ -43,14 +52,25 @@ $('#message-form').on('submit', function(e) {
 var locationButton = $('#send-location');
 
 socket.on('newLocationMessage', function(message) {
-    var li = $('<li></li>');
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var a = $('<a target="_blank">My location</a>');
 
-    li.text(`${message.from} ${formattedTime}: `);;
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+
+    // var li = $('<li></li>');
+    // var formattedTime = moment(message.createdAt).format('h:mm a');
+    // var a = $('<a target="_blank">My location</a>');
+
+    // li.text(`${message.from} ${formattedTime}: `);;
+    // a.attr('href', message.url);
+    // li.append(a);
+    // $('#messages').append(li);
 });
 locationButton.on('click', function(e) {
     if (!navigator.geolocation) {
