d971f61612f011748b2d17b770c8928f2ea0a4c3
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-07-04 17:34:39 -0400

    add autoscroll for chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();
+
+function scrollToBottom() {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if (clientHeight + scrollTop + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
 socket.on('connect', function() {
     // console.info('connected to the server');
 
@@ -29,6 +43,7 @@ socket.on('newMessage', function(message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
     // var li = $('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
@@ -62,6 +77,8 @@ socket.on('newLocationMessage', function(message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
+
 
     // var li = $('<li></li>');
     // var formattedTime = moment(message.createdAt).format('h:mm a');
